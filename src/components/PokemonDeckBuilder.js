ff1411477bff80b24c34ca58c6fb50cf57da7eb7
Joey Zelinka <cheflife365@gmail.com>
2021-09-10 18:10:59 -0400

    ugh paths again

--- src/components/PokemonDeckBuilder.js
+++ src/components/PokemonDeckBuilder.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '/Users/joeyz/Desktop/w3B d3V/Dc C0d3/DCProjects/wordeckbuilder/src/styles.css'
+import '../styles.css';
 
 export default function PokemonDeckBuilder() {
     return (
